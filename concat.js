bceb8d0005e2bbe12e1c006592d819fbca73db70
masudweb <58891153+masudweb@users.noreply.github.com>
2020-09-22 02:07:04 +0600

    Create concat.js

---
+++ concat.js
@@ -0,0 +1,7 @@
+const first = [12, 34];
+const second = [98, 34, 63, 57];
+const third = [28, 31, 24, 300];
+
+const all = first.concat(second).concat(third);
+
+// result for all [12, 34, 98, 34, 63, 57, 28, 31, 24, 300]
